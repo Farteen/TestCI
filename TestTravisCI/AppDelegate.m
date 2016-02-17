bf90ccacefd856828cb021a098f99e66b7bf09cd
FarTeen <934628017@qq.com>
2016-02-17 11:11:39 +0800

    test push trigger travis

--- TestTravisCI/AppDelegate.m
+++ TestTravisCI/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    something_wrong_happened();
     return YES;
 }
 
